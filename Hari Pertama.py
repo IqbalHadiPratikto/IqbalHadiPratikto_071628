b6f41f27b7ebcf0b2b8825c7498df3de0bde9da9
unknown <iqbal.hadi.pratikto@gmail.com>
2022-06-04 13:17:14 +0700

    Initial Commit add Sesi 1

---
+++ Hari Pertama.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 09:51:28 2022
+
+@author: LENOVO
+"""
+
+#Hari Pertama#
+
+#print(123123123123123123123123123123123123123123123123 + 1)
+
+#print(10)
+#print(type(10))
+
+#print(4.2)
+#print(type(4.2))
+#print(4.)
+#print(.2)
+#print(.4e7)
+#print(4.2e-4)
+
+#print("Hacktiv8")
+#print(type("Hacktiv8"))
+
+#print(type(True))
+#print(type(False))
+
+#n = 300
+#print(n)
+
+#Contoh latihan Luas Segitiga
+#a=10
+#t=20
+#L=0.5*a*t
+#print(L)
+
+#a = b = c = 300
+#print(a, b, c)
+
+#var = 23.5
+#print(var)
+#var = "Now I'm a string"
+#print(var)
+
+#name = "Hacktiv8"
+#Age = 54
+#has_laptops = True
+#print(name, Age, has_laptops)
+
+#age = 1
+#Age = 2
+#aGe = 3
+#AGE = 4
+#a_g_e = 5
+#_age = 6
+#age_ = 7
+#_AGE_ = 8
+#print(age, Age, aGe, AGE, a_g_e, _age, age_, _AGE_)
+
+#a = 10
+#b = 20
+#a + b - 5
+
+#a = 10
+#b = 20
+#print(a+b*5)
+
+#a = 4
+#b = 3
+
+#print(a + b)
+#print(a - b)
+#print(a * b)
+#print(a / b)
+#print(a % b)
+#print(a ** b)
+
+#a = 10
+#b = 20
+#print(a == b)
+#print(a != b)
+#print(a <= b)
+#print(a >= b)
+
+#a = 30
+#b = 30
+#print(a == b)
+#print(a <= b)
+#print(a >= b)
+
+#s = 'foo'
+#t = 'bar'
+#u = 'baz'
+#print(s + t)
+#print(s + t + u)
+#print('Hacktiv8 ' + 'PTP')
+
