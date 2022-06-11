d98d40052b6ba5f9c41b8d4ffd1bafb4ffb26526
unknown <iqbal.hadi.pratikto@gmail.com>
2022-06-11 12:01:47 +0700

    add Sesi 3 and Readme

--- Hari Pertama Sesi 2.py
+++ Hari Pertama Sesi 2.py
@@ -7,3 +7,101 @@ Created on Sat Jun  4 13:56:14 2022
 
 #Hari Pertama Sesi 2#
 
+#x = 0
+#y = 5
+
+#if x < y:                            # Truthy
+#    print('yes')
+#if y < x:                            # Falsy
+#    print('yes')
+#if x:                                # Falsy
+#    print('yes')
+#if y:                                # Truthy
+#    print('yes')
+#if 'aul' in 'grault':                # Truthy
+#    print('yes')
+#if 'quux' in ['foo', 'bar', 'baz']:  # Falsy
+#    print('yes')
+
+#if 'foo' in ['bar', 'baz', 'qux']:
+#    print('Expression was true')
+#    print('Executing statement in suite')
+#    print('...')
+#    print('Done.')  
+#print('After conditional')
+
+#if 'foo' in ['foo', 'bar', 'baz']:        #  x
+#    print('Outer condition is true')      #  x
+#    if 10 > 20:                           #  x
+#        print('Inner condition 1')        #        x
+#    print('Between inner conditions')     #  x
+#    if 10 < 20:                           #  x
+#        print('Inner condition 2')        #  x
+#    print('End of outer condition')       #  x
+#print('After outer condition')            #  x
+
+#x = 20
+#if x < 50:
+#    print('(first suite)')
+#    print('x is small')
+#else:
+#    print('(second suite)')
+#    print('x is large')
+    
+#x = 120
+#if x < 50:
+#    print('(first suite)')
+#    print('x is small')
+#else:
+#    print('(second suite)')
+#    print('x is large')
+
+#hargaBuku = 20000
+#hargaMajalah = 5000
+#uang = 2000
+#if uang > hargaBuku:
+#    print("beli buku")
+#else:
+#    print("uang tidak cukup")
+    
+#Latihan
+#print("Kalkulator Belanja\n")
+#HargaBuku = input ("Masukkan Harga Buku Yang Ingin Dibeli = Rp.")
+#JumlahBeli = input ("Masukkan Jumlah Beli = ")
+#print ("Buku")
+#Uang = input ("Masukkan Uang yang Dimiliki = Rp.")
+#Total = int(HargaBuku) * int(JumlahBeli)
+#Kembalian = int(Uang) - int(Total)
+#if int(Total) < int(Uang):
+#    print ("Uang Sisa = Rp.", Kembalian)  
+#elif int(Total) == int(Uang):
+#    Print ("Allahmdulillah Kebeli Tapi Gak Ada Sisa")
+#else:
+#    print ("Gak Jadi Beli Uang Gak Cukup")
+
+
+#Latihan 2
+#print("Kalkulator Belanja\n")
+#Absen = input ("Input Nilai Absen =")
+#Tugas = input ("Input Nilai Tugas =")
+#UTS = input ("Input Nilai UTS =")
+#UAS = input ("Input Nilai UAS =")
+
+#NilaiAbsen = int(Absen) * 10/100
+#NilaiTugas = int(Tugas) * 20/100
+#NilaiUTS = int(UTS) * 30/100
+#NilaiUAS = int(UAS) * 40/100
+
+#NilaiTotal = NilaiAbsen + NilaiTugas + NilaiUTS + NilaiUAS
+
+#if NilaiTotal >= 80 <= 100:
+#    print("Anda Mendapatkan A")
+#elif NilaiTotal >= 70 <= 79:
+#    print ("Anda Mendapatkan B")
+#elif NilaiTotal >= 60 <= 69:
+#    print ("Anda Mendapatkan C")
+#elif NilaiTotal >= 50 <= 59:
+#    print("Anda Mendapatkan D")
+#else:
+#    print("Anda Mendapatkan E")
+
