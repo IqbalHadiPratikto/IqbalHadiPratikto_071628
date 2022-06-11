2f54832a7702e32d64cbae078a40b889f86814db
unknown <iqbal.hadi.pratikto@gmail.com>
2022-06-11 16:18:24 +0700

    add Sesi 4

---
+++ Training_Python2/Hari Kedua Sesi 2.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 11 13:05:05 2022
+
+@author: LENOVO
+"""
+
+import numpy as np
+
+#a = np.array([[1 , 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+#b = np.zeros(6)
+#c = np.ones(6)
+#d = np.empty(6)
+
+#print (np.arange(4))
+#print (np.arange(0,10,2))
+#print (np.arange(2,29,5))
+
+#arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+#b = np.append(arr, [1,2])
+#c = np.delete(arr, 1)
+#d = np.sort(arr)
+#print (b)
+#print (c)
+#print (d)
+
+#array_example = np.array([[[0, 1, 2, 3],
+#                           [4, 5, 6, 7]],
+
+#                          [[0, 1, 2, 3],
+#                           [4, 5, 6, 7]],
+
+#                          [[0, 1, 2, 3],
+#                           [4, 5, 6, 7]]])
+
+#print(array_example)
+#print(array_example.ndim)
+#print(array_example.size)
+#print(array_example.shape)
+
+#arr_one = np.array([[1, 2, 3, 4, 5]])
+#print(arr_one.ndim)
+#print(arr_one.size)
+#print(arr_one.shape)
+
+#a = np.arange(6)
+#print(a)
+#b = a.reshape(3,2)
+#print(b)
+#c = a.reshape(6,1)
+#print (c)
+
+#a = np.array([1, 2, 3, 4, 5, 6])
+#print(a.shape)
+
+#a2 = a[np.newaxis]
+#print(a2.shape)
+#print(a2)
+
+#row_vector = a[np.newaxis, :]
+#print(row_vector.shape)
+#print(row_vector)
+
+#col_vector = a[:, np.newaxis]
+#print(col_vector.shape)
+#print(col_vector)
+
+#a = np.array([1, 2, 3, 4, 5, 6])
+#print(a.shape)
+#b = np.expand_dims(a, axis=1)
+#print(b.shape)
+#c = np.expand_dims(a, axis=0)
+#print(c.shape)
+
+#data = np.array([1,2,3])
+#print(data)
+#print(data[0])
+#print(data[1])
+#print(data[0:2])
+#print(data[1:])
+#print(data[-2:])
+
+
+#a = np.array([[1 , 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+#print(a)
+#print(a[a>=5])
+
+#five_up = (a >= 5)
+#print(a[five_up])
+#print(a[a>=5])
+
+#divisible_by_2 = a[a%2==0]
+#print(divisible_by_2)
+
+#c = a[(a > 2) & (a < 11)]
+#print(c)
+
+#arr = np.array([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10])
+#arr1 = arr[3:8]
+#print(arr1)
+
+#a_1 = np.array([[1, 1],
+#                [2, 2]])
+#a_2 = np.array([[3, 3],
+#                [4, 4]])
+#print(np.vstack((a_1, a_2)))
+#print(np.hstack((a_1, a_2)))
+
+#arrsplit = np.array([[1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12],
+#                     [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]])
+#print(arrsplit)
+#print(np.hsplit(arrsplit,3))
+
+#a = np.array([1, 2, 3, 4])
+#print(a.sum())
+#b = np.array([[1, 1], [2, 2]])
+#print(b.sum(axis=0))
+#print(b.sum(axis=1))
+
+#data = np.array([1, 2])
+#print(data)
+#ones = np.ones(2)
+#print(ones)
+#print(data + ones)
+#print(data * data)
+#print(data / data)
+#print(data * 2)
+
+#A = np.array([[0.45053314, 0.17296777, 0.34376245, 0.5510652],
+#              [0.54627315, 0.05093587, 0.40067661, 0.55645993],
+#              [0.12697628, 0.82485143, 0.26590556, 0.56917101]])
+#print(A)
+#print(A.sum())
+#print(A.min())
+#print(A.min(axis=0))
+#print(A.max())
+#print(A.max(axis=1))
+#print(A.std())
+
+#print(np.ones((3,2)))
+#print(np.zeros((3,2)))
+#print(np.random.random((3,2)))
+
+#data = np.array([[1, 2], [3, 4]])
+#print(data)
+
+#ones = np.ones([2, 2])
+#print(ones)
+
+#print(data + ones)
+
+#ones_row = np.ones([1, 2])
+#print(ones_row)
+
+#print(data + ones_row)
+
+
+
+
